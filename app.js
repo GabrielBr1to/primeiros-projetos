bde30622eeb50e000ac3dddc4d3f1a762f60f20e
Gabriel Brito <168463368+GabrielBr1to@users.noreply.github.com>
2026-03-03 22:52:24 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+alert ('Bem vindo ao nosso site!');
+
+let nome = ("lua");
+let idade = (25);
+
+let numeroVendas = (50);
+let saldoDisponivel = (1000);
+
+
+let digiteNome = prompt ("Qual seu nome?");
+let digiteIdade = prompt ("Qual sua idade?");
+
+let mensagemErro = ("Erro! Preencha todos os campos");
+   alert(mensagemErro);
+
+let Years = prompt("digite sua idade!");
+   if (Years >= 18) {
+      alert ("pode tirar a habilitação!")
+   };
+
